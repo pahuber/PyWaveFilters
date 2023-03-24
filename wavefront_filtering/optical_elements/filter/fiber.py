4786790d92ab0132570cdb1ed99fc44d80697460
pahuber <philhuber@live.com>
2023-03-24 10:37:34 +0100

    Add coupling efficiency, core radius validation and apply method

--- wavefront_filtering/optical_elements/filter/fiber.py
+++ wavefront_filtering/optical_elements/filter/fiber.py
@@ -1,8 +1,13 @@
+import warnings
+
+import astropy
 import numpy as np
+from astropy import units as u
 from scipy.optimize import fsolve
 
 from wavefront_filtering.optical_elements.filter.bessel_equation import get_system_of_equations, \
     get_mode_function
+from wavefront_filtering.optical_elements.lens import Lens
 from wavefront_filtering.optical_elements.optical_element import OpticalElement
 from wavefront_filtering.wavefronts.wavefront import BaseWavefront
 
@@ -18,7 +23,8 @@ class Fiber(OpticalElement):
                  cladding_radius: float,
                  core_refractive_index: float,
                  cladding_refractive_index: float,
-                 wavefront: BaseWavefront):
+                 wavefront: BaseWavefront,
+                 lens: Lens):
         '''
         Constructor for fiber object.
 
@@ -29,20 +35,46 @@ class Fiber(OpticalElement):
                         core_refractive_index: Refractive index of the core
                         cladding_refractive_index: Refractive index of the cladding
                         wavefront: Base wavefront object
+                        lens: Lens used for coupling
 
         '''
+        self.wavefront = wavefront
+        self.lens = lens
         self.intended_wavelength = intended_wavelength
         self.core_radius = core_radius
         self.cladding_radius = cladding_radius
         self.core_refractive_index = core_refractive_index
         self.cladding_refractive_index = cladding_refractive_index
-        self.wavefront = wavefront
-        description = f'Fiber with core radius {self.core_radius}, cladding radius {self.cladding_radius},' \
-                      f'core refractive index {self.core_refractive_index}, cladding refractive index' \
-                      f'{cladding_refractive_index} and intended wavelength {self.intended_wavelength}.'
+        self.description = f'Fiber with core radius {self.core_radius}, cladding radius {self.cladding_radius},' \
+                           f'core refractive index {self.core_refractive_index}, cladding refractive index' \
+                           f'{cladding_refractive_index} and intended wavelength {self.intended_wavelength}.'
 
         self.v_number = self.get_v_number()
         self.fundamental_fiber_mode = self.get_fundamental_fiber_mode()
+        self.coupling_efficiency = self.get_coupling_efficiency()
+
+    @property
+    def core_radius(self) -> float:
+        '''
+        Return the core radius.
+
+                Returns:
+                        Float corresponding to core radius
+        '''
+        return self._core_radius
+
+    @core_radius.setter
+    def core_radius(self, value):
+        '''
+        Setter method for the core radius.
+        '''
+        if not (type(value) == astropy.units.quantity.Quantity and value.unit == u.meter):
+            raise ValueError(f'Units of core radius must be specified in meters.')
+        if self.wavefront.array_width_focal_plane_dimensionless * self.lens.focal_length / 2 < value:
+            raise ValueError(
+                f'core diameter {2 * value} must be smaller than the wavefront array width in meters '
+                f'{self.wavefront.array_width_focal_plane_dimensionless * self.lens.focal_length}')
+        self._core_radius = value
 
     def get_v_number(self):
         v_number = 2 * np.pi / self.intended_wavelength * self.core_radius * \
@@ -57,10 +89,11 @@ class Fiber(OpticalElement):
                                         (np.sqrt(self.v_number).value, np.sqrt(self.v_number).value),
                                         self.v_number.value)
         # normalization_constant = fsolve(get_orthogonality_equations, 1, (u_variable, w_variable, self.core_radius,
-        #
-        #
-        linspace = np.linspace(-self.cladding_radius, self.cladding_radius, self.wavefront.array_dimension)
-        X, Y = np.meshgrid(linspace, linspace)
+        # TODO: add normalization
+
+        extent = self.wavefront.array_width_focal_plane_dimensionless * self.lens.focal_length / 2
+        extent_linear_space = np.linspace(-extent, extent, self.wavefront.array_dimension)
+        X, Y = np.meshgrid(extent_linear_space, extent_linear_space)
 
         angles = np.arctan2(Y, X)
         radii = np.sqrt(X ** 2 + Y ** 2)
@@ -71,9 +104,29 @@ class Fiber(OpticalElement):
             for y in range(wavefront_shape[1]):
                 fundamental_fiber_mode[x][y] = get_mode_function(radii[x][y], angles[x][y], u_variable, w_variable,
                                                                  self.core_radius)
-                # TODO: crop map to cladding radius
 
         return fundamental_fiber_mode
 
-    def apply(self):
-        pass
+    def get_coupling_efficiency(self):
+        coupling_efficiency = np.sum(
+            abs(self.fundamental_fiber_mode.conjugate() * self.wavefront.complex_amplitude) ** 2) / (
+                                      np.sum(abs(self.fundamental_fiber_mode) ** 2) * np.sum(
+                                  abs(self.wavefront.complex_amplitude) ** 2))
+        # TODO: check coupling efficiency definition
+        if coupling_efficiency <= 0.7:
+            warnings.warn(f'Coupling efficiency is only {coupling_efficiency * 100} %')
+        if coupling_efficiency > 0.85:
+            warnings.warn(
+                f'Coupling efficiency {coupling_efficiency * 100} % is larger than the theoretical limit.'
+                f'Something must be wrong')
+
+        return coupling_efficiency
+
+    def apply(self, wavefront: BaseWavefront):
+        if not (self.wavefront == wavefront):
+            raise Exception('Fiber must be applied to the same wavefront that was used to initialize it')
+        else:
+            if not wavefront.is_pupil_plane:
+                wavefront.complex_amplitude = self.fundamental_fiber_mode * self.coupling_efficiency
+            else:
+                raise Exception('Fibers can only be applied to wavefronts in the focal plane')
