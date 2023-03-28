3aac2605c62d206e92b717c890168657e0964d9c
pahuber <philhuber@live.com>
2023-03-28 12:10:20 +0200

    Remove unused part

--- wavefront_filtering/optical_elements/filter/bessel.py
+++ wavefront_filtering/optical_elements/filter/bessel.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy import integrate
 from scipy.special import jv, kv
 
 
@@ -27,15 +26,3 @@ def get_mode_function(radius: float,
         angular_part = np.sin(bessel_order * angle)
 
     return radial_part * angular_part
-
-
-def get_orthogonality_equations(variable,
-                                u_variable: float,
-                                w_variable: float,
-                                core_radius: float,
-                                cladding_radius: float):
-    normalization_constant = variable
-    integrand = lambda r, phi: np.conj(get_mode_function(r, phi, u_variable, w_variable, core_radius)) * \
-                               get_mode_function(r, phi, u_variable, w_variable, core_radius)
-
-    return normalization_constant ** 2 * integrate.dblquad(integrand, 0, 2 * np.pi, 0, cladding_radius)[0] - 1
