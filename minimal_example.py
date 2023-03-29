c052dbed425e888ad0ee01efc3032a01e5175b84
pahuber <philhuber@live.com>
2023-03-29 18:57:29 +0200

    Update example to include fiber

--- minimal_example.py
+++ minimal_example.py
@@ -1,37 +1,56 @@
 from astropy import units as u
 from matplotlib import pyplot as plt
 
+from wavefront_filtering.optical_elements.filter.fiber import Fiber
 from wavefront_filtering.optical_elements.filter.pinhole import Pinhole
 from wavefront_filtering.optical_elements.lens import Lens
 from wavefront_filtering.wavefronts.wavefront import Wavefront
 
 # Define wavefront
 wavelength = 1e-5 * u.meter
-initial_intensity = 1 * u.watt ** 0.5 / u.meter
-zernike_modes = [(5, 0 * wavelength / 10)]
+initial_intensity = 10 * u.watt ** 0.5 / u.meter
+zernike_modes = [(5, 0 * wavelength / 5)]
 aperture_diameter = 0.003 * u.meter
-array_dimension = 400
+number_of_pixels = 100
 
 wavefront = Wavefront(wavelength,
                       initial_intensity,
                       zernike_modes,
                       aperture_diameter,
-                      array_dimension)
+                      number_of_pixels)
 
 # Define optical elements
-lens_1 = Lens(100)
-lens_2 = Lens(100)
+focal_length = 0.008 * u.meter
+
+lens = Lens(focal_length)
+
 pinhole = Pinhole(1.22, wavefront)
 
-# Apply optical elements
-wavefront.apply(lens_1)
+fiber = Fiber(wavelength,
+              21e-6 * u.meter,
+              125e-6 * u.meter,
+              1.6,
+              1.59,
+              wavefront,
+              lens)
 
-plt.imshow(wavefront.intensity.value)
+# Plot fiber mode
+e = wavefront.array_width_focal_plane_dimensionless * focal_length.value / aperture_diameter * wavelength / 2
+plt.imshow(abs(fiber.fundamental_fiber_mode) ** 2, extent=[-e, e, -e, e])
+plt.colorbar()
+plt.show()
+
+# Apply optical elements
+wavefront.apply(lens)
+
+e = wavefront.array_width_focal_plane_length.value / 2
+plt.imshow(wavefront.intensity.value, extent=[-e, e, -e, e])
+plt.xticks([-2e-5, -1e-5, 0, 3.25e-5, 9.76e-5])
+plt.colorbar()
 plt.show()
 
 wavefront.apply(pinhole)
 
-plt.imshow(wavefront.intensity.value)
-plt.show()
+wavefront.apply(fiber)
 
-wavefront.apply(lens_2)
+print(fiber.coupling_efficiency)
