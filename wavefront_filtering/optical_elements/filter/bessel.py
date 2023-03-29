748f0a806a3977ceb6ec1e8ea92000c9d336e139
pahuber <philhuber@live.com>
2023-03-29 18:57:00 +0200

    Add dicumentation

--- wavefront_filtering/optical_elements/filter/bessel.py
+++ wavefront_filtering/optical_elements/filter/bessel.py
@@ -1,10 +1,20 @@
+from typing import Tuple
+
 import numpy as np
 from scipy.special import jv, kv
 
 
-def get_system_of_equations(variables, v_number):
+def get_system_of_equations(variables: Tuple, v_number: float) -> float:
+    '''
+    Used to solve the system of equations with scipy fsolve.
+
+            Parameters:
+                    variables: Tuple of variables
+                    v_number: Float corresponding to the V-number of the fiber
+    '''
     u_variable, w_variable = variables
     bessel_order = 0
+
     return (v_number - np.sqrt(u_variable ** 2 + w_variable ** 2), (u_variable * jv(bessel_order + 1, u_variable)) /
             (jv(bessel_order, u_variable)) - (w_variable * kv(bessel_order + 1, w_variable)) / (
                 kv(bessel_order, w_variable)))
@@ -15,7 +25,21 @@ def get_mode_function(radius: float,
                       u_variable: float,
                       w_variable: float,
                       core_radius: float,
-                      even=True):
+                      even=True) -> float:
+    '''
+    Return the value of the mode function at a given position.
+
+            Parameters:
+                    radius: Radial coordinate value
+                    angle: Angular coordinate value
+                    u_variable: Variable corresponding to variable u
+                    w_variable: Variable corresponding to w
+                    core_radius: Core radius in meters
+                    even: Boolean specifying whether to use the even or odd solution
+
+            Returns:
+                    The value of the mode function for a given position
+    '''
     bessel_order = 0
     radial_part = np.heaviside(core_radius - radius, 0) * jv(bessel_order, u_variable * radius / core_radius) / \
                   jv(bessel_order, u_variable) + np.heaviside(radius - core_radius, 0) * \
