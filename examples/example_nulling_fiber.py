364468bfce8c485d0e8939cdd2564db8947aa656
pahuber <philhuber@live.com>
2023-03-31 14:39:49 +0200

    Update example

--- examples/example_nulling_fiber.py
+++ examples/example_nulling_fiber.py
@@ -1,16 +1,16 @@
 import numpy as np
 from astropy import units as u
 
-from wavefront_filtering.optical_elements.filter.fiber_optics.fiber import Fiber
+from wavefront_filtering.optical_elements.filter.fiber import Fiber
 from wavefront_filtering.optical_elements.lens import Lens
 from wavefront_filtering.wavefronts.wavefront import Wavefront
 
 # Define wavefront
-wavelength = 1e-5 * u.meter
-zernike_modes_1 = [(5, wavelength / 1000)]
-zernike_modes_2 = [(6, wavelength / 1000)]
+wavelength = 15e-6 * u.meter
+zernike_modes_1 = [(5, wavelength / 100)]
+zernike_modes_2 = [(6, wavelength / 100)]
 beam_diameter = 0.003 * u.meter
-number_of_pixels = 100
+number_of_pixels = 4000  # Warning: null depth is sensitive to low pixel values
 
 wavefront_1 = Wavefront(wavelength,
                         zernike_modes_1,
@@ -23,24 +23,16 @@ wavefront_2 = Wavefront(wavelength,
                         number_of_pixels)
 
 # Define optical elements
-focal_length = 0.008 * u.meter
+focal_length = 0.003 * u.meter
 lens = Lens(focal_length)
-fiber_1 = Fiber(wavelength,
-                beam_diameter,
-                21e-6 * u.meter,
-                125e-6 * u.meter,
-                1.6,
-                1.59,
-                number_of_pixels,
-                lens)
-fiber_2 = Fiber(wavelength,
-                beam_diameter,
-                21e-6 * u.meter,
-                125e-6 * u.meter,
-                1.6,
-                1.59,
-                number_of_pixels,
-                lens)
+fiber = Fiber(wavelength,
+              beam_diameter,
+              12e-6 * u.meter,
+              170e-6 * u.meter,
+              2.7,
+              2.6587,
+              number_of_pixels,
+              lens)
 
 # Calculate null depth without filtering
 wavefront_const_unfiltered = wavefront_1 + wavefront_2
@@ -54,8 +46,8 @@ print('Unfiltered Null Depth: ', null_depth_unfiltered)
 wavefront_1.apply(lens)
 wavefront_2.apply(lens)
 
-wavefront_1.apply(fiber_1)
-wavefront_2.apply(fiber_2)
+wavefront_1.apply(fiber)
+wavefront_2.apply(fiber)
 
 wavefront_1.apply(lens)
 wavefront_2.apply(lens)
