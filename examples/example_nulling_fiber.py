f80731643b85307612e9155fc12914aa5df6a85b
pahuber <philhuber@live.com>
2023-04-03 18:02:03 +0200

    Adapt examples

--- examples/example_nulling_fiber.py
+++ examples/example_nulling_fiber.py
@@ -10,7 +10,7 @@ wavelength = 15e-6 * u.meter
 zernike_modes_1 = [(5, wavelength / 100)]
 zernike_modes_2 = [(6, wavelength / 100)]
 beam_diameter = 0.003 * u.meter
-number_of_pixels = 4000  # Warning: null depth is sensitive to low pixel values
+number_of_pixels = 400
 
 wavefront_1 = Wavefront(wavelength,
                         zernike_modes_1,
@@ -23,41 +23,48 @@ wavefront_2 = Wavefront(wavelength,
                         number_of_pixels)
 
 # Define optical elements
-focal_length = 0.003 * u.meter
+focal_length = 0.074 * u.meter
 lens = Lens(focal_length)
 fiber = Fiber(wavelength,
               beam_diameter,
-              12e-6 * u.meter,
-              170e-6 * u.meter,
+              2 * 12e-6 * u.meter,
+              2 * 170e-6 * u.meter,
               2.7,
-              2.6587,
+              2.6987,
               number_of_pixels,
               lens)
 
-# Calculate null depth without filtering
-wavefront_const_unfiltered = wavefront_1 + wavefront_2
-wavefront_dest_unfiltered = wavefront_1 - wavefront_2
-intensity_unfiltered = wavefront_const_unfiltered.intensity
-
-null_depth_unfiltered = np.sum(wavefront_dest_unfiltered.intensity) / np.sum(wavefront_const_unfiltered.intensity)
-print('Unfiltered Null Depth: ', null_depth_unfiltered)
-
-# Apply optical elements to wavefronts
-wavefront_1.apply(lens)
-wavefront_2.apply(lens)
-
-wavefront_1.apply(fiber)
-wavefront_2.apply(fiber)
-
-wavefront_1.apply(lens)
-wavefront_2.apply(lens)
-
-# Interfere wavefronts and calculate null depth and throughput
+# Combine wavefronts and normalize
 wavefront_const = wavefront_1 + wavefront_2
 wavefront_dest = wavefront_1 - wavefront_2
-intensity_filtered = wavefront_const.intensity
 
+intensity_unfiltered = wavefront_const.intensity
+
+# Apply optical elements to wavefronts
+wavefront_const.apply(lens)
+wavefront_dest.apply(lens)
+
+# plt.plot(wavefront_const.intensity.value[number_of_pixels // 2], label='co')
+# plt.plot(wavefront_dest.intensity.value[number_of_pixels // 2], label='de')
+# plt.plot((abs(fiber.fundamental_fiber_mode) ** 2).value[number_of_pixels // 2], label='fiber')
+# plt.legend()
+# plt.show()
+
+wavefront_const.apply(fiber)
+wavefront_dest.apply(fiber)
+
+# plt.plot(wavefront_const.intensity.value[number_of_pixels // 2], label='co')
+# plt.plot(wavefront_dest.intensity.value[number_of_pixels // 2], label='de')
+# plt.plot((abs(fiber.fundamental_fiber_mode) ** 2).value[number_of_pixels // 2], label='fiber')
+# plt.legend()
+# plt.show()
+
+wavefront_1.apply(lens)
+wavefront_2.apply(lens)
+
+# Calculate null depth and throughput
 null_depth = np.sum(wavefront_dest.intensity) / np.sum(wavefront_const.intensity)
-throughput = np.sum(intensity_filtered) / np.sum(intensity_unfiltered)  # TODO: check units and values
-print('Filtered Null Depth: ', null_depth)
+intensity_filtered = wavefront_const.intensity
+throughput = np.sum(intensity_filtered) / np.sum(intensity_unfiltered)
+print('Null Depth: ', null_depth)
 print('Throughput: ', throughput)
--- examples/example_nulling_pinhole.py
+++ examples/example_nulling_pinhole.py
@@ -6,11 +6,11 @@ from wavefront_filtering.optical_elements.lens import Lens
 from wavefront_filtering.wavefronts.wavefront import Wavefront
 
 # Define wavefront
-wavelength = 1e-5 * u.meter
-zernike_modes_1 = [(5, wavelength / 1000)]
-zernike_modes_2 = [(6, wavelength / 1000)]
+wavelength = 15e-6 * u.meter
+zernike_modes_1 = [(5, 0 * wavelength / 100)]
+zernike_modes_2 = [(6, wavelength / 100)]
 beam_diameter = 0.003 * u.meter
-number_of_pixels = 100
+number_of_pixels = 400
 
 wavefront_1 = Wavefront(wavelength,
                         zernike_modes_1,
@@ -23,34 +23,39 @@ wavefront_2 = Wavefront(wavelength,
                         number_of_pixels)
 
 # Define optical elements
-focal_length = 0.008 * u.meter
+focal_length = 0.003 * u.meter
 lens = Lens(focal_length)
 pinhole = Pinhole(1.22, beam_diameter, number_of_pixels)
 
-# Calculate null depth without filtering
-wavefront_const_unfiltered = wavefront_1 + wavefront_2
-wavefront_dest_unfiltered = wavefront_1 - wavefront_2
-intensity_unfiltered = wavefront_const_unfiltered.intensity
-
-null_depth_unfiltered = np.sum(wavefront_dest_unfiltered.intensity) / np.sum(wavefront_const_unfiltered.intensity)
-print('Unfiltered Null Depth: ', null_depth_unfiltered)
-
-# Apply optical elements to wavefronts
-wavefront_1.apply(lens)
-wavefront_2.apply(lens)
-
-wavefront_1.apply(pinhole)
-wavefront_2.apply(pinhole)
-
-wavefront_1.apply(lens)
-wavefront_2.apply(lens)
-
-# Interfere wavefronts and calculate null depth and throughput
+# Combine wavefronts and normalize
 wavefront_const = wavefront_1 + wavefront_2
 wavefront_dest = wavefront_1 - wavefront_2
-intensity_filtered = wavefront_const.intensity
 
+intensity_unfiltered = wavefront_const.intensity
+
+# Apply optical elements to wavefronts
+wavefront_const.apply(lens)
+wavefront_dest.apply(lens)
+
+# plt.plot(wavefront_const.intensity.value[number_of_pixels // 2], label='co')
+# plt.plot(wavefront_dest.intensity.value[number_of_pixels // 2], label='de')
+# plt.legend()
+# plt.show()
+
+wavefront_const.apply(pinhole)
+wavefront_dest.apply(pinhole)
+#
+# plt.plot(wavefront_const.intensity.value[number_of_pixels // 2], label='co')
+# plt.plot(wavefront_dest.intensity.value[number_of_pixels // 2], label='de')
+# plt.legend()
+# plt.show()
+
+wavefront_const.apply(lens)
+wavefront_dest.apply(lens)
+
+# Calculate null depth and throughput
 null_depth = np.sum(wavefront_dest.intensity) / np.sum(wavefront_const.intensity)
-throughput = np.sum(intensity_filtered) / np.sum(intensity_unfiltered)  # TODO: check units and values
-print('Filtered Null Depth: ', null_depth)
+intensity_filtered = wavefront_const.intensity
+throughput = np.sum(intensity_filtered) / np.sum(intensity_unfiltered)
+print('Null Depth: ', null_depth)
 print('Throughput: ', throughput)
