20c1c0d271f5d6ac96085a883c2a8d2d5e2864aa
pahuber <philhuber@live.com>
2023-04-13 08:56:17 +0200

    Update example

--- examples/example_nulling_fiber.py
+++ examples/example_nulling_fiber.py
@@ -8,7 +8,7 @@ from pywavefilters.wavefronts.wavefront import Wavefront
 # Define wavefront
 wavelength = 15e-6 * u.meter
 zernike_modes_1 = [(5, wavelength / 100)]
-zernike_modes_2 = [(6, wavelength / 100)]
+zernike_modes_2 = [(6, wavelength / 200)]
 beam_diameter = 0.003 * u.meter
 number_of_pixels = 200
 
@@ -34,31 +34,23 @@ fiber = Fiber(wavelength,
               number_of_pixels,
               lens)
 
+intensity_unfiltered = (wavefront_1 + wavefront_2).intensity
+
+# Transform to focal plane and apply fibers
+wavefront_1.apply(lens)
+wavefront_2.apply(lens)
+wavefront_1.apply(fiber)
+wavefront_2.apply(fiber)
+
+# Remove piston difference with delay line
+piston_diff = np.angle(wavefront_1.complex_amplitude) - np.angle(wavefront_2.complex_amplitude)
+wavefront_1.complex_amplitude *= np.exp(-1j * piston_diff.value)
+
 # Combine wavefronts and normalize
 wavefront_const = wavefront_1 + wavefront_2
 wavefront_dest = wavefront_1 - wavefront_2
 
-intensity_unfiltered = wavefront_const.intensity
-
-# Apply optical elements to wavefronts
-wavefront_const.apply(lens)
-wavefront_dest.apply(lens)
-
-# plt.plot(wavefront_const.intensity.value[number_of_pixels // 2], label='co')
-# plt.plot(wavefront_dest.intensity.value[number_of_pixels // 2], label='de')
-# plt.plot((abs(fiber.fundamental_fiber_mode) ** 2).value[number_of_pixels // 2], label='fiber')
-# plt.legend()
-# plt.show()
-
-wavefront_const.apply(fiber)
-wavefront_dest.apply(fiber)
-
-# plt.plot(wavefront_const.intensity.value[number_of_pixels // 2], label='co')
-# plt.plot(wavefront_dest.intensity.value[number_of_pixels // 2], label='de')
-# plt.plot((abs(fiber.fundamental_fiber_mode) ** 2).value[number_of_pixels // 2], label='fiber')
-# plt.legend()
-# plt.show()
-
+# Transform to output plane
 wavefront_const.apply(lens)
 wavefront_dest.apply(lens)
 
